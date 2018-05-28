df2fbea0515c8789e3038337945d37119ad26e6f
Ben Krikler <b.krikler@cern.ch>
2018-05-28 17:27:28 +0200

    Update the nanoAOD crab config

--- ZH/crab_cfgs/crab_submission_NanoAOD.py
+++ ZH/crab_cfgs/crab_submission_NanoAOD.py
@@ -11,7 +11,8 @@ def auto_find_request_name(directory, name, index=1):
 
 config = config()
 config.General.workArea        = 'crab_ZH_ZToQQ_Hinv_M125'
-config.General.requestName     = auto_find_request_name(config.General.workArea,'ZH_ZToQQ_Hinv_M125_NanoAOD')
+requestName     = auto_find_request_name(config.General.workArea,'ZH_ZToQQ_Hinv_M125_NanoAOD')
+config.General.requestName     = requestName
 config.General.transferOutputs = True
 config.General.transferLogs    = True
 
@@ -19,6 +20,7 @@ config.JobType.pluginName = 'ANALYSIS'
 config.JobType.psetName   = '/afs/cern.ch/work/b/bkrikler/chia/HinvProduction/ZH/cmsdriver_cfgs/driver_cfg_nanoaod.py'
 config.JobType.outputFiles = ['output_nanoaod.root']
 config.JobType.numCores = 4
+config.JobType.maxJobRuntimeMin = 120
 
 config.Data.inputDataset         = '/ZH_ZToQQ_Hinv_M125/bkrikler-ZH_ZToQQ_Hinv_M125_MiniAOD-28028af67189b3de7224b79195bd0e1d/USER' # Update this
 config.Data.inputDBS             = 'phys03'
@@ -27,7 +29,7 @@ config.Data.unitsPerJob          = 1
 config.Data.totalUnits           = -1
 config.Data.outLFNDirBase        = '/store/user/%s/' % (getUsernameFromSiteDB())
 config.Data.publication          = True
-config.Data.outputDatasetTag     = config.General.requestName
+config.Data.outputDatasetTag     = requestName
 
 config.Site.storageSite = 'T2_UK_London_IC'
 #config.Site.storageSite = 'T2_UK_SGrid_Bristol'
