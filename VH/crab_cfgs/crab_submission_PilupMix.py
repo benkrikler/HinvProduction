0acd002d0d4a88524426fde313740ed223c8a028
Ben Krikler <b.krikler@cern.ch>
2018-09-20 11:16:39 +0200

    Add crab submission scripts

---
+++ VH/crab_cfgs/crab_submission_PilupMix.py
@@ -0,0 +1,35 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+
+
+def auto_find_request_name(directory, name, index=1):
+    import os
+    full = "{}_{}".format(name, index)
+    if not os.path.isdir(os.path.join(directory, "crab_" + full)):
+        return full
+    return auto_find_request_name(directory, name, index + 1)
+
+
+config = config()
+config.General.workArea        = 'crab_%%KERNEL%%'
+requestName     = auto_find_request_name(config.General.workArea,'%%KERNEL%%_PUMix')
+config.General.requestName     = requestName
+config.General.transferOutputs = True
+config.General.transferLogs    = True
+
+config.JobType.pluginName = 'ANALYSIS'
+config.JobType.psetName   = '%%DRIVER%%'
+config.JobType.outputFiles = ['output_premix_interm.root']
+config.JobType.numCores = 1
+config.JobType.maxMemoryMB = 2500
+
+config.Data.inputDataset         = '%%INPUT_DATASET%%' # Update this
+config.Data.inputDBS             = 'phys03'
+config.Data.splitting            = 'EventAwareLumiBased'
+config.Data.unitsPerJob          = 30
+config.Data.totalUnits           = -1
+config.Data.outLFNDirBase        = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.publication          = True
+config.Data.outputDatasetTag     = requestName
+
+config.Site.storageSite = 'T2_UK_London_IC'
+#config.Site.storageSite = 'T2_UK_SGrid_Bristol'
---
+++ VH/prepare_crab.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python3
+"""
+"""
+import os
+import string
+import re
+
+ScriptDir = os.path.realpath(os.path.dirname(__file__))
+CrabCfgDir = "crab_cfgs"
+
+
+def process_args():
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument("-m", "--prod-mode", help="Which Production mode to generate for", choices=("ZH", "WplusH", "WminusH"), required=True)
+    parser.add_argument("-b", "--build-dir", help="What is the 'build' directory to use.  Defaults to 'build_{prod-mode}' if not given.", default=None)
+    parser.add_argument("-s", "--stage", help="Which stage in the data processing to run")
+    parser.add_argument("-r", "--replacements", action="append", help="Add a list of variables to replace in the input config file.  Must take form: 'KEY=Value'", default=[])
+    args = parser.parse_args()
+    if not args.build_dir:
+        args.build_dir = "build_" + args.prod_mode
+    args.build_dir = os.path.realpath(args.build_dir)
+    args.crab_dir = os.path.join(args.build_dir, CrabCfgDir) 
+    return args
+
+
+def setup(build_dir, crab_dir):
+    os.makedirs(build_dir, exist_ok=True)
+    if not os.path.isdir(crab_dir):
+        os.path.mkdir(crab_dir)
+
+
+def make_config(stage, crab_dir, prod_mode, build_dir, replacements):
+    repls = dict(Kernel = prod_mode + "_Vhadronic_Hinv_M125",
+                 Driver = os.path.join(build_dir, "driver_cfg_{stage}.py").format(stage=stage)
+                )
+    for repl in replacements:
+        key, value = repl.split("=")
+        repls[key] = value
+    in_cfg = os.path.join(ScriptDir, CrabCfgDir, "crab_submission_{stage}.py").format(stage=stage)
+    out_cfg = os.path.join(crab_dir, "crab_submission_{stage}.py").format(stage=stage)
+    print("Input Crab config template:", in_cfg)
+    print("Output Crab config:", out_cfg)
+    print("Replacements:", repls)
+
+    with open(in_cfg, "r") as infile:
+        lines = infile.read()
+
+    for key, repl in repls.items():
+        lines = re.sub("%%" + key.upper() + "%%", repl, lines)
+
+    with open(out_cfg, "w") as outfile:
+        outfile.write(lines)
+
+
+if __name__ == "__main__":
+    args = process_args()
+    setup(args.build_dir, args.crab_dir)
+    make_config(**vars(args))
